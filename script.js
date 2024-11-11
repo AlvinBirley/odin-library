c5859e0d5be2d699baa79a65f617923923106434
AlvinBirley <alvinbirley@gmail.com>
2024-11-11 13:25:41 -0600

    Build modal for user input

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 //array of all the books in my library
-const myLibrary = ["LOTR", "Bible", "The Illiad", ["book1", "book2", "book3"]];
+const myLibrary = []
+
+//selecting the moadl 
+const modal = document.querySelector(".modal")
+
+//selecting the add book button to open the modal 
+const modalBtn = document.querySelector(".btn")
+
+//select the backdrop div
+const backdrop = document.querySelector(".backdrop");
+
+
+const addBtn = document.getElementById(".add")
+
+modalBtn.addEventListener('click', () => {
+    modal.style.display = "flex";
+    backdrop.style.display = "block"; // Show the backdrop
+    setTimeout(() => {
+        modal.classList.add('show'); 
+    }, 10);
+
+   
+    document.body.classList.add('modal-open');
+})
 
 //book conscructor
 function Book(title, author, pages, read) {
@@ -7,10 +30,15 @@ function Book(title, author, pages, read) {
   this.author=author;
   this.pages=pages;
   this.read=read;
-}
 
+  this.toString = function() {
+    return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`
+  }
+}
+//fucntion to create new book object and push it the myLibrary array 
 function addBookToLibrary() {
-  // do stuff here
+  let bookOne = new Book("LOTR", "Tolkien", 543, "have not read")
+  myLibrary.push(bookOne)
 }
 
 //function that loops over myLibrary and updates cells with book info 
@@ -26,4 +54,21 @@ else {
 }
 }
 
+addBookToLibrary();
+console.log(myLibrary)
 upDateCell();
+
+//function to capture field values of modal
+function getModalInfo () {
+const titleValue = document.getElementById("title").value;
+    console.log(titleValue)
+    const authorValue = document.getElementById("author").value;
+    console.log(authorValue)
+    const pagesValue = document.getElementById("pages").value;
+    console.log(pagesValue)
+    const readValue = document.getElementById("read").value;
+    console.log(readValue)
+}
+
+//event listener on modal button to grab field values
+document.getElementById("add-info").addEventListener("click", getModalInfo);
