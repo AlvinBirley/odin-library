27d200df75e58ebf7f987585ecf4b88427078bcf
AlvinBirley <alvinbirley@gmail.com>
2024-11-10 19:53:20 -0600

    Add function to add books to library

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const myLibrary = [];
+//array of all the books in my library
+const myLibrary = ["LOTR", "Bible", "The Illiad", ["book1", "book2", "book3"]];
 
 function Book() {
   // the constructor...
@@ -8,3 +9,17 @@ function addBookToLibrary() {
   // do stuff here
 }
 
+//function that loops over myLibrary and updates cells with book info 
+function upDateCell (){
+for (let i=0; i<myLibrary.length; i++) {
+    let addToLib= document.querySelector(`td[data-value="${i+1}"]`)
+if(addToLib){
+    addToLib.innerHTML=myLibrary[i]
+}
+else {
+    console.log("This cell doesn't exist.")
+}
+}
+}
+
+upDateCell();
